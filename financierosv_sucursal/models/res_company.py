620e709530d091e34475d54217197b079e9fc577
chevox7 <keab55@hotmail.com>
2022-04-28 15:25:33 -0600

    terminando libro mayor11

--- financierosv_sucursal/models/res_company.py
+++ financierosv_sucursal/models/res_company.py
@@ -70,7 +70,7 @@ from
 (
 select aa.code
     ,aa.name
-    ,(select COALESCE(sum(aml1.debit),0) - COALESCE(sum(aml1.credit),0) 
+    ,case when {3}=1 then (select COALESCE(sum(aml1.debit),0) - COALESCE(sum(aml1.credit),0) 
         from account_move_line aml1
         inner join account_move am1 on aml1.move_id=am1.id
         inner join account_account a1 on aml1.account_id=a1.id
