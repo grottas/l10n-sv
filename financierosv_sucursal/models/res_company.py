18ea805f286716af94ca9b3442e18ac6b46fe58a
chevox7 <keab55@hotmail.com>
2022-05-03 10:58:49 -0600

    arreglando el resultado2

--- financierosv_sucursal/models/res_company.py
+++ financierosv_sucursal/models/res_company.py
@@ -348,7 +348,7 @@ order by S2.code
         where aa2.company_id={0} and aa2.code like aa.code ||'%' and date_part('month',COALESCE(am2.date,am2.invoice_date))={2} and am2.state in ('posted') ) as haber3
 
 from cuentas aa 
-where aa.company_id= {0} and length(trim(aa.code))=4 and aa.code like '4102%'
+where aa.company_id= {0} and length(trim(aa.code))=4 and aa.code between '4100%' and  '4106%'
 order by aa.code
 )S2
 where S2.previo3<>0 or S2.debe3<>0 or S2.haber3<>0 
