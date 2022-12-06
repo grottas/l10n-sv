6272e466e68322b6d586aacccdd83d3a73aa8a81
Nestor Ulloa <nestoreumma@gmail.com>
2022-12-06 09:16:45 -0600

    se agregan las modificaciones para quitar los detalles del combo en el ticket y se agrega el modulo que afecta la precision del monto de la configuracion del impuesto

---
+++ decimal_precision_amount/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Decimal precision amount',
+    'category': 'account',
+    'summary': '',
+    'description': "Cambio de precision decimal para el monto en la configuracion de impuestos",
+    'version': '15.0.1.0',
+    'license': 'GPL-3',
+    'author': "Expertha(Nestor Ulloa)",
+    'depends': ['base'],
+    'data': [
+
+    ],
+    'assets': {
+        'web.assets_qweb': [
+
+        ],
+        'point_of_sale.assets':[
+
+        ]
+    },
+    'installable': True,
+}
---
+++ decimal_precision_amount/models/account_tax.py
@@ -0,0 +1,7 @@
+from odoo import fields, models, api
+
+
+class AccountTax(models.Model):
+    _inherit = 'account.tax'
+
+    amount = fields.Float(digits=(12, 2))
