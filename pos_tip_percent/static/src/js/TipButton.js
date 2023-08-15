68ef12d2e8b48433d0289b8ddcdb85c9dfabddec
Expertha <100881758+Expertha@users.noreply.github.com>
2023-08-15 14:41:38 -0600

    quitando el 13%

--- pos_tip_percent/static/src/js/TipButton.js
+++ pos_tip_percent/static/src/js/TipButton.js
@@ -81,7 +81,7 @@ odoo.define('pos_tip_percent.TipButton', function(require) {
                     base_to_tip = order.get_total_with_tax();
                 }
             }
-            var tip = tpc / 100.0 * (base_to_tip + (base_to_tip * 0.13) - discount_value - descuento) ;
+            var tip = tpc / 100.0 * (base_to_tip + (base_to_tip) - discount_value - descuento) ;
 
             if( tip > 0 ){
                 await order.add_product(product, {
